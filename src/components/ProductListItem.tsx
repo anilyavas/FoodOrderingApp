790b7eea7218cf803fbd2256dcf0f6ceb0e0b6c1
Anıl <anil@Anl-MacBook-Air.local>
2024-02-07 12:25:42 +0300

    routing between pages

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,26 +1,28 @@
-import { StyleSheet, View, Text, Image } from 'react-native';
-import products from '@assets/data/products';
+import { StyleSheet, View, Text, Image, Pressable } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { Product } from '../../types';
+import { Link } from 'expo-router';
 
 type ProductListItemProps = {
   product: Product;
 };
 const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    <View style={styles.container}>
-      <Image
-        source={{
-          uri:
-            product.image ||
-            'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png',
-        }}
-        style={styles.image}
-        resizeMode='contain'
-      />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
-    </View>
+    <Link href={'/product'} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{
+            uri:
+              product.image ||
+              'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png',
+          }}
+          style={styles.image}
+          resizeMode='contain'
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+      </Pressable>
+    </Link>
   );
 };
 
@@ -29,11 +31,14 @@ export default ProductListItem;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    borderRadius: 20,
+    backgroundColor: 'white',
+    padding: 10,
+    maxWidth: '50%',
   },
   image: {
     width: '100%',
     aspectRatio: 1,
-    alignSelf: 'center',
   },
   title: {
     fontWeight: '600',
