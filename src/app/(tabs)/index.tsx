c279d65a597df6de8dfa0c74393e068027fa0c39
Anıl <anil@Anl-MacBook-Air.local>
2024-02-07 12:07:26 +0300

    file paths and ProductListItem

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,21 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
+import { StyleSheet, View } from 'react-native';
+import products from '@assets/data/products';
+import ProductListItem from '@/components/ProductListItem';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <ProductListItem product={products[0]} />
+      <ProductListItem product={products[1]} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 10,
+    overflow: 'hidden',
   },
 });
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,45 @@
+import { StyleSheet, View, Text, Image } from 'react-native';
+import products from '@assets/data/products';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { Product } from '../../types';
+
+type ProductListItemProps = {
+  product: Product;
+};
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <View>
+      <Image
+        source={{
+          uri:
+            product.image ||
+            'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png',
+        }}
+        style={styles.image}
+        resizeMode='contain'
+      />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+    alignSelf: 'center',
+  },
+  title: {
+    fontWeight: '600',
+    fontSize: 18,
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+    marginTop: 'auto',
+  },
+});
