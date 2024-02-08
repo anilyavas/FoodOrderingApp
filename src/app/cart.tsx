c609799c859c1f0ec0a6b220d5d699b321303d9c
Anıl <anil@Anl-MacBook-Air.local>
2024-02-08 12:37:23 +0300

    shopping cart finished

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,10 +1,11 @@
-import { View, Text, Platform, FlatList } from 'react-native';
+import { View, Platform, FlatList, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useCart } from '@/providers/CartProvider';
 import CartListItem from '@/components/CartListItem';
+import Button from '@/components/Button';
 
 const CartScreen = () => {
-  const { items } = useCart();
+  const { items, total } = useCart();
   return (
     <View>
       <FlatList
@@ -12,6 +13,10 @@ const CartScreen = () => {
         renderItem={({ item }) => <CartListItem cartItem={item} />}
         contentContainerStyle={{ padding: 10, gap: 10 }}
       />
+      <Button text='Checkout' />
+      <Text style={{ marginTop: 20, fontSize: 20, fontWeight: '500' }}>
+        Total: ${total}
+      </Text>
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
   );
--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -6,12 +6,14 @@ export const CartContext = createContext<CartType>({
   items: [],
   addItem: () => {},
   updateQuantity: () => {},
+  total: 0,
 });
 
 type CartType = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem['size']) => void;
   updateQuantity: (itemId: string, amount: -1 | 1) => void;
+  total: number;
 };
 
 const CartProvider = ({ children }: PropsWithChildren) => {
@@ -19,6 +21,13 @@ const CartProvider = ({ children }: PropsWithChildren) => {
 
   const addItem = (product: Product, size: CartItem['size']) => {
     // if already in cart, increment quantitiy
+    const existingItem = items.find(
+      (item) => item.product === product && item.size === size
+    );
+    if (existingItem) {
+      updateQuantity(existingItem.id, 1);
+      return;
+    }
 
     const newCartItem: CartItem = {
       id: randomUUID(), // generate
@@ -42,9 +51,15 @@ const CartProvider = ({ children }: PropsWithChildren) => {
         .filter((item) => item.quantity > 0)
     );
   };
+  const total = items.reduce(
+    (sum, item) => (sum += item.product.price * item.quantity),
+    0
+  );
 
   return (
-    <CartContext.Provider value={{ items: items, addItem, updateQuantity }}>
+    <CartContext.Provider
+      value={{ items: items, addItem, updateQuantity, total }}
+    >
       {children}
     </CartContext.Provider>
   );
