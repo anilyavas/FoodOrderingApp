434f97b07fb7daf54c778030353cbb6f2525f75f
Anıl <anil@Anl-MacBook-Air.local>
2024-02-08 14:20:08 +0300

    create a product

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -4,6 +4,7 @@ import Button from '@/components/Button';
 import { defaultPizzaImage } from '@/components/ProductListItem';
 import Colors from '@/constants/Colors';
 import * as ImagePicker from 'expo-image-picker';
+import { Stack } from 'expo-router';
 
 const CreateProductScreen = () => {
   const [name, setName] = useState('');
@@ -57,6 +58,7 @@ const CreateProductScreen = () => {
   };
   return (
     <View style={styles.container}>
+      <Stack.Screen options={{ title: 'Create Product' }} />
       <Image
         source={{ uri: image || defaultPizzaImage }}
         style={styles.image}
