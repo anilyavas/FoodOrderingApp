2802bf8e592ba74430fc0250fee9e41d384cafd8
Anıl <anil@Anl-MacBook-Air.local>
2024-02-08 14:13:19 +0300

    image picking on create screen

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,12 +1,16 @@
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Image } from 'react-native';
 import { useState } from 'react';
 import Button from '@/components/Button';
+import { defaultPizzaImage } from '@/components/ProductListItem';
+import Colors from '@/constants/Colors';
+import * as ImagePicker from 'expo-image-picker';
 
 const CreateProductScreen = () => {
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
 
   const [errors, setErrors] = useState('');
+  const [image, setImage] = useState<string | null>(null);
 
   const resetFields = () => {
     setName('');
@@ -39,8 +43,27 @@ const CreateProductScreen = () => {
 
     resetFields();
   };
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
   return (
     <View style={styles.container}>
+      <Image
+        source={{ uri: image || defaultPizzaImage }}
+        style={styles.image}
+      />
+      <Text onPress={pickImage} style={styles.textButton}>
+        Select Image
+      </Text>
       <Text style={styles.label}>Name</Text>
       <TextInput
         value={name}
@@ -80,5 +103,15 @@ const styles = StyleSheet.create({
   label: {
     color: 'grey',
     fontSize: 16,
+  },
+  image: {
+    width: '50%',
+    aspectRatio: 1,
+    alignSelf: 'center',
+  },
+  textButton: {
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    color: Colors.light.tint,
   },
 });
