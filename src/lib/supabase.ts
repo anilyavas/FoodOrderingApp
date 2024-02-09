b940cf83a481cdea02e83fa29cd9d56263124fe7
Anıl <anil@Anl-MacBook-Air.local>
2024-02-09 14:19:35 +0300

    supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,28 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://lzjovbybmjuoqvcpqyqn.supabase.co';
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6am92YnlibWp1b3F2Y3BxeXFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0NzcwNzQsImV4cCI6MjAyMzA1MzA3NH0.7t2XciWaIh5S6renJSohSJhdFSWUrvO9AXwRy2GyarI';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
