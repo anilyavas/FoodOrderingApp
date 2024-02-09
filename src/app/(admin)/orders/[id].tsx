73b97e36284cee3238525af0a6bbdb83a770b4df
Anıl <anil@Anl-MacBook-Air.local>
2024-02-09 13:28:05 +0300

    order status for admin panel

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -1,8 +1,10 @@
 import OrderItemListItem from '@/components/OrderItemListItem';
 import OrderListItem from '@/components/OrderListItem';
+import Colors from '@/constants/Colors';
 import orders from '@assets/data/orders';
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { FlatList, Text, View } from 'react-native';
+import { FlatList, Pressable, Text, View } from 'react-native';
+import { OrderStatusList } from 'types';
 
 const OrderDetailsScreen = () => {
   const { id } = useLocalSearchParams();
@@ -18,6 +20,39 @@ const OrderDetailsScreen = () => {
         contentContainerStyle={{ gap: 10 }}
         data={order.order_items}
         renderItem={({ item }) => <OrderItemListItem item={item} />}
+        ListFooterComponent={() => (
+          <>
+            <Text style={{ fontWeight: 'bold' }}>Status</Text>
+            <View style={{ flexDirection: 'row', gap: 5 }}>
+              {OrderStatusList.map((status) => (
+                <Pressable
+                  key={status}
+                  onPress={() => console.warn('Update status')}
+                  style={{
+                    borderColor: Colors.light.tint,
+                    borderWidth: 1,
+                    padding: 10,
+                    borderRadius: 5,
+                    marginVertical: 10,
+                    backgroundColor:
+                      order.status === status
+                        ? Colors.light.tint
+                        : 'transparent',
+                  }}
+                >
+                  <Text
+                    style={{
+                      color:
+                        order.status === status ? 'white' : Colors.light.tint,
+                    }}
+                  >
+                    {status}
+                  </Text>
+                </Pressable>
+              ))}
+            </View>
+          </>
+        )}
       />
     </View>
   );
