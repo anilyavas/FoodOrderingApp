6d82d5c77e2dab53a7ba11b6d6a4d0c8907ea586
Anıl <anil@Anl-MacBook-Air.local>
2024-02-10 13:37:44 +0300

    admin side auth

--- src/app/(admin)/_layout.tsx
+++ src/app/(admin)/_layout.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Tabs } from 'expo-router';
+import { Redirect, Tabs } from 'expo-router';
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
+import { useAuth } from '@/providers/AuthProvider';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -14,6 +15,11 @@ function TabBarIcon(props: {
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const { isAdmin } = useAuth();
+
+  if (!isAdmin) {
+    return <Redirect href={'/'} />;
+  }
 
   return (
     <Tabs
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,13 +5,16 @@ import { useAuth } from '@/providers/AuthProvider';
 import { supabase } from '@/lib/supabase';
 
 const index = () => {
-  const { session, loading } = useAuth();
+  const { session, loading, isAdmin } = useAuth();
 
   if (loading) {
     return <ActivityIndicator />;
   }
   if (!session) {
     return <Redirect href={'/sign-in'} />;
+  }
+  if (!isAdmin) {
+    return <Redirect href={'/(user)'} />;
   }
 
   return (
@@ -22,9 +25,7 @@ const index = () => {
       <Link href={'/(admin)'} asChild>
         <Button text='Admin' />
       </Link>
-      <Link href={'/sign-in'} asChild>
-        <Button text='Sign in' />
-      </Link>
+
       <Button onPress={() => supabase.auth.signOut()} text='Sign out' />
     </View>
   );
