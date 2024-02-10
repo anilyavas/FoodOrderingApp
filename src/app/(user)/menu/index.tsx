6d4624400d3cbe4297a52f42d9e8fcb232f10f83
Anıl <anil@Anl-MacBook-Air.local>
2024-02-10 14:21:08 +0300

    crud operations

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -1,11 +1,30 @@
-import { FlatList } from 'react-native';
+import { ActivityIndicator, FlatList } from 'react-native';
 import products from '@assets/data/products';
 import ProductListItem from '@/components/ProductListItem';
+import { supabase } from '@/lib/supabase';
+import { useQuery } from '@tanstack/react-query';
 
-export default function TabOneScreen() {
+export default function MenuScreen() {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['products'],
+    queryFn: async () => {
+      const { data, error } = await supabase.from('products').select('*');
+      if (error) {
+        throw new Error(error.message);
+      }
+      return data;
+    },
+  });
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+  if (error) {
+    return <Text>Failed to fetch products</Text>;
+  }
+
   return (
     <FlatList
-      data={products}
+      data={data}
       renderItem={({ item }) => <ProductListItem product={item} />}
       numColumns={2}
       contentContainerStyle={{ gap: 10, padding: 10 }}
